ecb6f6af9826f860087db802f65d2cadf67d41b4
万佳阳 <wanjiayang@wanjiayangdembp.workgroup>
2016-06-21 22:24:33 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by 万佳阳 on 16/6/21.
+//  Copyright © 2016年 万佳阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by 万佳阳 on 16/6/21.
+//  Copyright © 2016年 万佳阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
